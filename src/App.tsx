430d07587d832ac400e047088607eec46b44c403
bhargav3738 <nimbalkarbhargav19@gmail.com>
2026-07-26 13:08:21 -0400

    feat: port navbar and mobile menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { UIProvider } from "./context/UIContext";
 import { CartProvider } from "./context/CartContext";
+import { Navbar } from "./components/layout/Navbar";
 import { Toast } from "./components/ui/Toast";
 
 export default function App() {
   return (
     <UIProvider>
       <CartProvider>
+        <Navbar />
         <main id="main-view" />
         <Toast />
       </CartProvider>
